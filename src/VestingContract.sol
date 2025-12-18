cb324e88b94e2c0ce253780218cf48c5a65bb94d
Tolulope Malomo <malomo.tolz@gmail.com>
2025-12-18 09:00:41 +0100

    add checks for createVestingSchedule function

--- src/VestingContract.sol
+++ src/VestingContract.sol
@@ -6,13 +6,13 @@ import {Ownable} from "@openzeppelin/access/Ownable.sol";
 import {IERC20} from "@openzeppelin/token/ERC20/IERC20.sol";
 
 /**
-* @title Vesting Contract
-* @notice Manages locked and released Vestify tokens
+ * @title Vesting Contract
+ * @notice Manages locked and released Vestify tokens
  */
 contract VestingContract is Ownable {
     // Types
     struct VestingSchedule {
-        address user;
+        address userAddress;
         uint256 startTimestamp;
         uint256 endTimestamp;
         uint256 cliffTimestamp;
@@ -21,42 +21,50 @@ contract VestingContract is Ownable {
     }
 
     // State variables
-    uint256 vestifyTokenBalance;
+    uint256 private vestifyTokenBalance;
     VestingSchedule[] private s_vestingScheduleList;
-    mapping(address => uint256) s_addressToVestingIdxPlusOne;
+    mapping(address => uint256) private s_addressToVestingIdxPlusOne;
+
+    // Errors
+    error VestingContract__AmountLessThanZero();
+    error VestingContract__InvalidVestingPeriod();
 
     constructor() Ownable(msg.sender) {}
 
     /**
-    * @notice Creates a new vesting schedule for a user
-    * @param user The address of the user receiving the vested tokens
-    * @param startTimestamp The timestamp when vesting begins
-    * @param endTimestamp The timestamp when vesting ends
-    * @param cliffTimestamp The timestamp before which no tokens can be released
-    * @param totalAmount The total amount of tokens to be vested
-    * @param releasedAmount The amount of tokens already released
+     * @notice Creates a new vesting schedule for a user
+     * @param user The address of the user receiving the vested tokens
+     * @param startTimestamp The timestamp when vesting begins
+     * @param endTimestamp The timestamp when vesting ends
+     * @param cliffTimestamp The timestamp before which no tokens can be released
+     * @param totalAmount The total amount of tokens to be vested
      */
     function createVestingSchedule(
-        address user,
+        address userAddress,
         uint256 startTimestamp,
         uint256 endTimestamp,
         uint256 cliffTimestamp,
-        uint256 totalAmount,
-        uint256 releasedAmount
+        uint256 totalAmount
     ) external onlyOwner {
+        if (totalAmount <= 0)
+            revert VestingContract__AmountNotGreaterThanZero();
+
+        if (startTimestamp > cliffTimestamp || cliffTimestamp > endTimestamp)
+            revert VestingContract__InvalidVestingPeriod();
+
         // Transfer vestify tokens from caller to this contract
         IERC20(address(0)).transferFrom(msg.sender, address(this), totalAmount);
 
-        VestingSchedule newVestingSchedule = VestingSchedule({
-            user,
-            startTimestamp, 
-            endTimestamp,
-            cliffTimestamp,
-            totalAmount,
-            releasedAmount
+        VestingSchedule memory newVestingSchedule = VestingSchedule({
+            userAddress: userAddress,
+            startTimestamp: startTimestamp,
+            endTimestamp: endTimestamp,
+            cliffTimestamp: cliffTimestamp,
+            totalAmount: totalAmount,
+            releasedAmount: 0
         });
 
-       s_addressToVestingIdxPlusOne[user] = s_vestingScheduleList.length + 1;
-       s_vestingScheduleList.push(newVestingSchedule);
+        s_addressToVestingIdxPlusOne[user] = s_vestingScheduleList.length + 1;
+        s_vestingScheduleList.push(newVestingSchedule);
     }
 }

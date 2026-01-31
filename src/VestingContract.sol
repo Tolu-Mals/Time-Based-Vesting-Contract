33bee62838cb5ae1f46e14fd137763c7c4811196
Tolulope Malomo <malomo.tolz@gmail.com>
2026-01-31 19:02:08 +0100

    small fix

--- src/VestingContract.sol
+++ src/VestingContract.sol
@@ -38,7 +38,8 @@ contract VestingContract is
     uint256 private s_nextIndexToProcess;
     uint256 private constant BATCH_SIZE = 10;
     uint256 private constant ONE_DAY_IN_SECONDS = 24 * 60 * 60;
-    mapping(address beneficiary => uint256 vestingScheduleIndex) private s_beneficiaryToVestingScheduleIndexPlusOne;
+    mapping(address beneficiary => uint256 vestingScheduleIndex)
+        private s_beneficiaryToVestingScheduleIndexPlusOne;
     VestingSchedule[] private s_vestingScheduleList;
 
     // Errors
@@ -56,7 +57,7 @@ contract VestingContract is
 
     /**
      * @notice Creates a new vesting schedule for a user
-     * @param beneficiaryAddress The address of the user receiving the vested tokens
+     * @param beneficiary The address of the user receiving the vested tokens
      * @param startTimestamp The timestamp when vesting begins
      * @param endTimestamp The timestamp when vesting ends
      * @param cliffTimestamp The timestamp before which no tokens can be released
@@ -94,7 +95,9 @@ contract VestingContract is
         });
 
         s_vestingScheduleList.push(newVestingSchedule);
-        s_beneficiaryToVestingScheduleIndexPlusOne[beneficiary] = s_vestingScheduleList.length; //we index + 1 becuase default mapping value is 0
+        s_beneficiaryToVestingScheduleIndexPlusOne[
+            beneficiary
+        ] = s_vestingScheduleList.length; //we index + 1 becuase default mapping value is 0
 
         emit VestingScheduleCreated(beneficiary);
     }
@@ -110,7 +113,7 @@ contract VestingContract is
         returns (bool upkeepNeeded, bytes memory performData)
     {
         uint256 count = 0;
-        uint256 startIndex = s_nextIndexToProces;
+        uint256 startIndex = s_nextIndexToProcess;
         uint256 nextIndexToProcess;
         uint256[] memory idsToProcess = new uint256[](
             s_vestingScheduleList.length
@@ -133,12 +136,12 @@ contract VestingContract is
                 count = count + 1;
             }
 
-            if(count == BATCH_SIZE - 1){
+            if (count == BATCH_SIZE - 1) {
                 nextIndexToProcess = i + 1;
             }
 
             //If we've run through all the schedules, reset to 0
-            if(i == s_vestingScheduleList.length - 1){
+            if (i == s_vestingScheduleList.length - 1) {
                 nextIndexToProcess = 0;
             }
         }
@@ -157,16 +160,19 @@ contract VestingContract is
         }
     }
 
-//TODO: The Docs mentioned something about any body being able to call performUpkeep, so we need to do some checks, in this case someone could change the value of s_nextIndexToProcess or idsToProcess, to get rewards faster
+    //TODO: The Docs mentioned something about any body being able to call performUpkeep, so we need to do some checks, in this case someone could change the value of s_nextIndexToProcess or idsToProcess, to get rewards faster
     function performUpkeep(bytes calldata performData) external override {
-        (uint256[] memory idsToProcess, uint256 nextIndexToProcess ) = abi.decode(performData, (uint256[], uint256));
+        (uint256[] memory idsToProcess, uint256 nextIndexToProcess) = abi
+            .decode(performData, (uint256[], uint256));
         s_nextIndexToProcess = nextIndexToProcess;
 
         for (uint256 i = 0; i < idsToProcess.length; i++) {
-            uint256 amountToRelease = s_vestingScheduleList[idsToProcess[i]].amountPerDay;
+            uint256 amountToRelease = s_vestingScheduleList[idsToProcess[i]]
+                .amountPerDay;
 
             if (
-                s_vestingScheduleList[idsToProcess[i]].releasedAmount + amountToRelease >
+                s_vestingScheduleList[idsToProcess[i]].releasedAmount +
+                    amountToRelease >
                 s_vestingScheduleList[idsToProcess[i]].totalAmount
             ) {
                 amountToRelease =
@@ -174,28 +180,41 @@ contract VestingContract is
                     s_vestingScheduleList[idsToProcess[i]].releasedAmount;
             }
 
-            s_vestingScheduleList[idsToProcess[i]].releasedAmount += amountToRelease;
-            s_vestingScheduleList[idsToProcess[i]].lastTimestamp = block.timestamp;
+            s_vestingScheduleList[idsToProcess[i]]
+                .releasedAmount += amountToRelease;
+            s_vestingScheduleList[idsToProcess[i]].lastTimestamp = block
+                .timestamp;
         }
     }
 
     function withdrawFunds(uint256 amount) external {
-        uint256 vestingScheduleIndexPlusOne = s_beneficiaryToVestingScheduleIndexPlusOne[msg.sender];
+        uint256 vestingScheduleIndexPlusOne = s_beneficiaryToVestingScheduleIndexPlusOne[
+                msg.sender
+            ];
 
-        if(vestingScheduleIndexPlusOne == 0) revert VestingContract__BeneficiaryDoesNotExist();                
-        if(s_vestingScheduleList[vestingScheduleIndexPlusOne - 1].cliffTimestamp > block.timestamp) revert VestingContract__CliffPeriodNotReached();
+        if (vestingScheduleIndexPlusOne == 0)
+            revert VestingContract__BeneficiaryDoesNotExist();
+        if (
+            s_vestingScheduleList[vestingScheduleIndexPlusOne - 1]
+                .cliffTimestamp > block.timestamp
+        ) revert VestingContract__CliffPeriodNotReached();
 
-        uint256 amountAvailable = s_vestingScheduleList[vestingScheduleIndexPlusOne - 1].releasedAmount - s_vestingScheduleList[vestingScheduleIndexPlusOne - 1].withdrawnAmount;
+        uint256 amountAvailable = s_vestingScheduleList[
+            vestingScheduleIndexPlusOne - 1
+        ].releasedAmount -
+            s_vestingScheduleList[vestingScheduleIndexPlusOne - 1]
+                .withdrawnAmount;
 
-        if(amount > amountAvailable){
+        if (amount > amountAvailable) {
             revert VestingContract__InsufficientFunds();
         }
 
-        s_vestingScheduleList[vestingScheduleIndexPlusOne - 1].withdrawnAmount += amount;
+        s_vestingScheduleList[vestingScheduleIndexPlusOne - 1]
+            .withdrawnAmount += amount;
 
         // Transfer vestify tokens from this contract to beneficiary
         IERC20(address(0)).transferFrom(address(this), msg.sender, amount);
-        
+
         emit WithdrawalCompleted(msg.sender);
     }
 }
